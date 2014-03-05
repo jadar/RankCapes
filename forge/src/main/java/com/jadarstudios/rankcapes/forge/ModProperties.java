6047d8d8911a13243b0b3e36c5c6242cd1198b22
Jacob Rhoda <osx86geek@gmail.com>
2014-03-05 10:22:38 -0600

    removed things not needed for GH Pages.

--- forge/src/main/java/com/jadarstudios/rankcapes/forge/ModProperties.java
+++
@@ -1,8 +0,0 @@
-package com.jadarstudios.rankcapes.forge;
-
-public class ModProperties
-{
-    public static final String MOD_ID = "rankcapes";
-    public static final String MOD_NAME = "RankCapes";
-    public static final String MOD_VERSION = "1.0-SNAPSHOT";
-}
