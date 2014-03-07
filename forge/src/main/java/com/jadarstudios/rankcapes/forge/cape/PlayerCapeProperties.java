df9497f4b01271fb3a3717c670bb5203da6f477e
Jake Rhoda <jake@jadarstudios.com>
2014-03-07 00:11:53 -0600

    copyright

--- forge/src/main/java/com/jadarstudios/rankcapes/forge/cape/PlayerCapeProperties.java
+++ forge/src/main/java/com/jadarstudios/rankcapes/forge/cape/PlayerCapeProperties.java
@@ -1,3 +1,11 @@
+/**
+ * RankCapes Forge Mod
+ *
+ * Copyright (c) 2013 Jacob Rhoda.
+ * Released under the MIT license
+ * http://github.com/jadar/RankCapes/blob/master/LICENSE
+ */
+
 package com.jadarstudios.rankcapes.forge.cape;
 
 import net.minecraft.client.entity.AbstractClientPlayer;
--- forge/src/main/java/com/jadarstudios/rankcapes/forge/handler/PlayerEventHandler.java
+++ forge/src/main/java/com/jadarstudios/rankcapes/forge/handler/PlayerEventHandler.java
@@ -1,3 +1,11 @@
+/**
+ * RankCapes Forge Mod
+ *
+ * Copyright (c) 2013 Jacob Rhoda.
+ * Released under the MIT license
+ * http://github.com/jadar/RankCapes/blob/master/LICENSE
+ */
+
 package com.jadarstudios.rankcapes.forge.handler;
 
 import com.jadarstudios.rankcapes.forge.cape.PlayerCapeProperties;
