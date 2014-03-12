a0b2c6a17302f037ea872e60a707b76622d6f51d
Jake Rhoda <jake@jadarstudios.com>
2014-03-12 16:33:05 -0500

    testing

--- javascripts/build-page.js
+++ javascripts/build-page.js
@@ -1,18 +1,63 @@
 function getPageConfiguration()
 {
-    return $("#builds-properties");
+    var config = {};
+    
+    config = JSON.parse($("input[name='config']").val());
+    
+    return config;
 }
 
-function getIndexContents(repositoryId)
+function getConfiguredArtifacts()
 {
+    var artifacts = {};
     
+    artifacts = JSON.parse($("input[name='builds-artifacts']").val());
+
+    return artifacts;
+}
+
+function getIndexContents(repoUrl, repositoryId, groupId, artifactId)
+{
+    repoUrl = repoUrl + '/service/local/repositories/' + repositoryId + '/index_content/' + (groupId.replace(/\./g, "/")) + '/';
+    $.ajax({
+        type: 'GET',
+        url: repoUrl,
+        processData: true,
+        data: {},
+        dataType: "json",
+        success: 
+            function(data) {
+                processData(data);
+            }
+    });
+    
+//    $.getJSON(repoUrl, function(data)
+//    {
+//        console.log("success!");
+//        console.loc(data);
+//    });
+}
+
+function processData(data)
+{
+    console.log(data);
 }
 
 function onPageLoad()
 {
-//    $("#builds").html("Hello jQuery");
-//    console.log(getPageConfiguration().form);
+    var config = getPageConfiguration();
+    var artifacts = getConfiguredArtifacts();
+    if(config == null || artifacts == null) 
+        return;
+    
+    var index = {};
+    var i = 0;
+    for(var artifact in artifacts)
+    {
+        artifact = artifacts[artifact];
+        index[i] = getIndexContents(config["repo-url"], artifact["repo"], artifact["groupId"], artifact["artifactId"]);
+        i++;
+    }
+    
 }
 $(document).ready(onPageLoad);
-
-console.log("HelloWorld!");
