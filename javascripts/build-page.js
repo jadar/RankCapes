695678a1c985e447aa362b2a62cf828e7bc9e46e
Jake Rhoda <jake@jadarstudios.com>
2014-03-12 12:14:44 -0500

    some more hooks and some artifacts

---
+++ javascripts/build-page.js
@@ -0,0 +1,18 @@
+function getPageConfiguration()
+{
+    return $("#builds-properties");
+}
+
+function getIndexContents(repositoryId)
+{
+    
+}
+
+function onPageLoad()
+{
+//    $("#builds").html("Hello jQuery");
+//    console.log(getPageConfiguration().form);
+}
+$(document).ready(onPageLoad);
+
+console.log("HelloWorld!");
